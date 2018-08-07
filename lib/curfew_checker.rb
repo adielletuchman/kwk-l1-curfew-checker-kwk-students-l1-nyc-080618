7abe936a8403a9af79853438b5323099fdbb2957
adielletuchman <adielle.tuchman@mjbha.org>
2018-08-07 18:58:51 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,66 @@
+
 def simple_curfew_checker(time)
-  # code goes here
-end
+  if time >= 23
+    puts "You're in trouble! Better get home quick!"
+end 
+end 
+
+simple_curfew_checker (24)
 
 def curfew_checker(time)
-  # code goes here
-end
+if time >= 23
+  puts "You're in trouble! Better get home quick!"
+
+elsif time < 23
+puts "go have fun!"
+end 
+end 
+
+curfew_checker (23)
 
 def complex_curfew_checker(time)
-  # code goes here
-end
+  if time == 23
+    puts "time to apparate"
+    
+  elsif > 23
+  puts "you're in trouble! better get home quick!"
+  
+  elsif < 23
+  puts "keep having fun!!"
+  
+end 
+end 
+
+complex_curfew_checker (24)
 
 def deluxe_curfew_checker(time)
-  # code goes here
-end
+  if time == 23
+    puts "time to apparate!"
+  end 
+  
+  if time > 23
+    puts "you're in trouble! better get home quick!"
+  end 
+  
+  if time == 21
+    puts "you have 2 hours to keep having fun!"
+  end 
+end 
+
+deluxe_curfew_checker (23)
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
+ if time == 23
+   puts "Curfew is at 11, time to apparate"
 end
+
+elsif time > 23
+puts "you're in trouble! better be back to Hogwarts quick! Curfew was at 11."
+end 
+
+else time == 21
+  puts "You have 2 hours to have fun! Curfew at 11."
+end 
+end 
+
+platinum_curfew_checker (23)
